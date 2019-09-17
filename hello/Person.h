de1cda13f64f676aef5443f5da6bdba1e326d576
kinosoftwareengineer <1253109318@qq.com>
2019-09-17 14:13:34 +0800

    nihaokino

---
+++ hello/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  hello
+//
+//  Created by kino on 2019/9/17.
+//  Copyright © 2019 kino. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ hello/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  hello
+//
+//  Created by kino on 2019/9/17.
+//  Copyright © 2019 kino. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
